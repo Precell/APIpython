348cda812f727771dffcf84c2abeb3195a7ad659
Precell <82501476+Precell@users.noreply.github.com>
2022-02-22 15:11:25 +0200

    old env

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from flask_restful import Api
+from routes import BookList, Book, ReviewsList, Review
+from flask_cors import CORS
 
+BASE_URL = '/api/bookreactions'
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, flask world'
+api = Api(app)
+api.add_resource(BookList, f'{BASE_URL}/Books/')
+api.add_resource(Book, f'{BASE_URL}/Book/<book_id>')
+api.add_resource(ReviewsList, f'{BASE_URL}/Reviews/<book_id>')
+api.add_resource(Review, f'{BASE_URL}/Reviews/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ routes.py
@@ -0,0 +1,66 @@
+from pydoc import classname
+from urllib import request
+from webbrowser import get
+from flask_restful import Resource
+from repository import Repository
+from flask import request
+
+repo = Repository()
+
+
+class BookList(Resource):
+    def get(self):
+        return [book.__dict__ for book in repo.books_get_all()]
+    
+
+class Book(Resource):
+    def get(self, book_id):
+        return repo.book_get_by_id(int(book_id)).__dict__
+    
+    
+class ReviewsList(Resource):
+    def get(slef, book_id):
+        return [review.__dict__ for review in repo.reviews_get_by_book_id(int(book_id))]    
+    
+class Review(Resource):
+    def post(self):
+        data = request.get_json()
+        return repo.review_add(data).__dict__
+
+def post(self):
+    data = request.get_json()
+    return repo.book_add(data).__dict__ 
+   
+# class BookList(Resource):
+#     def get(self):
+#         return [book.__dict__ for book in repo.books_get_all()]
+    
+#     def post(self):
+#         data = request.get_json()
+#         return self.repo.book_add(data).__dict__
+    
+# class Book(Resource):
+#     def __init__(self, repo=repo):
+#         self.repo = repo
+        
+#     def get(self, book_id):
+#         return repo.book_get_by_id(int(book_id)).__dict__    
+    
+# class ReviewsList(Resource):
+#     def __init__(self, book_id):
+#         return [review.__dict__ for review in repo.reviews_get_by_id(int(book_id))]
+        
+#     def get(self):
+#         return [review.__dict__ for review in self.repo.review.get_all()]
+    
+#     def post(self):
+#         data = request.get_json()
+#         return self.repo.review_add(data).__dict__
+    
+# class Review(Resource):
+#     def post(self):
+#         data = request.get.json()
+#         return repo.review_add(data).__dict__            
+
+
+            
